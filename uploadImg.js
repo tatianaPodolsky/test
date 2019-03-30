f0814d2fd2a8c1f3f027e8ed207d7717616d37dc
Tatiana Podolsky <podolskaya.tatiana@gmail.com>
2019-03-30 14:35:42 +0300

    s

--- uploadImg.js
+++ uploadImg.js
@@ -4,10 +4,19 @@
    
 //  }           
 
-var file = document.getElementById('takePic').files[0]
-    var fr = new FileReader()
-    fr.readAsDataURL(file)
-    fr.onload = function(e) {
-      var img = document.querySelector(".img")
-      img.src = this.result
-    } 
+// var file = document.getElementById('takePic').files[0]
+//     var fr = new FileReader()
+//     fr.readAsDataURL(file)
+//     fr.onload = function(e) {
+//       var img = document.querySelector(".img")
+//       img.src = this.result
+//     } 
+  function previewFiles() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      document.getElementById('img').src = reader.result;
+    }
+    if (event.target.files[0]) {reader.readAsDataURL(event.target.files[0]);}
+    }
+
+  document.getElementById('takePic').onChange = previewFiles;
